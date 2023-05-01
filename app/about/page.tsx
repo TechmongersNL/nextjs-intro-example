94acbd430dc1950c4d0278bedcd43d97b377fb37
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2023-05-01 17:14:45 +0200

    feat(Styling): Use global, module, and styled components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,13 @@
+import {
+  Article,
+  Caption,
+  Heading1,
+  ImageWrapper,
+  Intro,
+  Paragraph,
+  StyledLink,
+} from "@/components/typography";
+
 import Image from "next/image";
 import Link from "next/link";
 import RecordCollectionImage from "/public/images/record-collection.jpeg";
@@ -14,39 +24,40 @@ export default function About() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <main>
-        <article className="max-w-3xl font-serif text-base leading-relaxed">
-          <h1 className="font-extrabold text-5xl mb-5 font-sans">About Us</h1>
+        <Article>
+          <Heading1>About Us</Heading1>
 
-          <p className="font-sans text-xl leading-loose mb-4 tracking-wide">
+          <Intro>
             We are recordstore. We sell records you can probably just stream
             from some place. But is it not much more fun to own a hard copy
             yourself?
-          </p>
+          </Intro>
 
-          <div className="relative aspect-video">
+          <ImageWrapper>
             <Image
               alt="A man in white pants searching through a record collection that covers an entire wall."
               src={RecordCollectionImage}
               fill
               placeholder="blur" // <--- this makes the image show blurred while it's loading
             />
-          </div>
+            <Caption>
+              A man in white pants searching through a record collection that
+              covers an entire wall.
+            </Caption>
+          </ImageWrapper>
 
-          <p className="mb-2">
+          <Paragraph>
             Find and buy the records you like through our generic website.
-          </p>
+          </Paragraph>
 
-          <p className="mb-2">
+          <Paragraph>
             Go back to the{" "}
-            <Link
-              className="font-semibold pointer-events-auto hover:underline"
-              href="/"
-            >
+            <StyledLink $as={Link} href="/">
               Homepage
-            </Link>
+            </StyledLink>
             .
-          </p>
-        </article>
+          </Paragraph>
+        </Article>
       </main>
     </div>
   );
